46c611a7a0d8b857db1ff79f69f0b39fdf55a785
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2026-05-21 22:03:02 +0300

    init commit

---
+++ src/main/java/com/abdullah/SmartInterviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.abdullah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInterviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInterviewSystemApplication.class, args);
+	}
+
+}
